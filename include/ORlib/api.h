1504396e16da8ea3ae989e82063b4f5d9e4a4147
Adin De'Rosier <adinderosier@live.com>
2025-02-04 03:26:05 -0800

    feat(PIDController): Added PID Controller and updated for const correctness

---
+++ include/ORlib/api.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// misc
+#include "ORlib/misc/logging/logger.hpp"
+#include "ORlib/misc/PID/pid_controller.hpp"
+#include "ORlib/misc/PID/pidf_controller.hpp"
+#include "ORlib/misc/PID/pida_controller.hpp"
+#include "ORlib/misc/math/math_utils.hpp"
+#include "ORlib/misc/math/vector2d.hpp"
